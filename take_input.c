0d072d7bc8ab039ab3d1018324a7bad23b87c385
Ayush Raj <ayushtheking33@gmail.com>
2021-08-30 12:23:58 +0530

    1st day

---
+++ take_input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter number a: ");
+    scanf("%d", &a);
+    printf("Number a is: %d",a);
+    return 0;
+}
---
+++ timid_army.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("ayush raj");
+    return 0;
+}
